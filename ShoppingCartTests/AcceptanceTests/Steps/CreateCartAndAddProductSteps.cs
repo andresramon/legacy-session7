5635cc7abc3561f178045c39498783b6b1bdfa3a
Andres Ramon <andres.ramon@gmail.com>
2022-10-06 12:50:16 +0200

    fix : broken test

--- ShoppingCartTests/AcceptanceTests/Steps/CreateCartAndAddProductSteps.cs
+++ ShoppingCartTests/AcceptanceTests/Steps/CreateCartAndAddProductSteps.cs
@@ -15,6 +15,7 @@ namespace ShoppingCartTests
     public class CreateCartAndAddProductSteps
     {
         private string customer;
+
         [Given(@"a empty shopping cart belonging to ""(.*)""")]
         public void GivenAEmptyShoppingCartBelongingTo(string customer)
         {
@@ -22,16 +23,16 @@ namespace ShoppingCartTests
             IShoppingCartRepository cartRepository = A.Fake<IShoppingCartRepository>();
             ICustomerRepository customerRepository = A.Fake<ICustomerRepository>();
             Customer customerOne = new Customer(customer);
-        
-            CreateEmptyCartUseCase useCase = new CreateEmptyCartUseCase(cartRepository,customerRepository);
+
+            CreateEmptyCartUseCase useCase = new CreateEmptyCartUseCase(cartRepository, customerRepository);
             A.CallTo(() => customerRepository.GetUserByName(customer)).Returns(customerOne);
             useCase.CreateFor(customer);
 
             A.CallTo(() =>
                 cartRepository.Create(A<ShoppingCart>.That.Matches(cart =>
                     cart.ToData().Customer.Name.Equals(customerOne.ToData().Name)
-                  && !cart.ToData().Products.Any()))).MustHaveHappened();
-            A.CallTo(() =>  customerRepository.GetUserByName(customer)).MustHaveHappened();
+                    && !cart.ToData().Products.Any()))).MustHaveHappened();
+            A.CallTo(() => customerRepository.GetUserByName(customer)).MustHaveHappened();
 
         }
 
@@ -41,17 +42,17 @@ namespace ShoppingCartTests
             string productCode = "VOUCHER-DEMO";
             decimal price = 9.99M;
             Product product = new Product(new Id());
-            product.Restore(new ProductData(new Id(),productCode,price));
+            product.Restore(new ProductData(new Id(), productCode, price));
             IShoppingCartRepository cartRepository = A.Fake<IShoppingCartRepository>();
             IProductRepository productRepository = A.Fake<IProductRepository>();
-        
-            ShoppingCart shoppingCart = new ShoppingCart(new Id(),new Customer(customer));
+
+            ShoppingCart shoppingCart = new ShoppingCart(new Id(), new Customer(customer));
             AddProductToCartUseCase useCase = new AddProductToCartUseCase(cartRepository, productRepository);
             A.CallTo(() => cartRepository.Get(shoppingCart.Id)).Returns(shoppingCart);
             A.CallTo(() => productRepository.GetByCode(productCode)).Returns(product);
-            useCase.AddProduct(shoppingCart.Id,productCode);
-            
-            A.CallTo(() =>  cartRepository.Save(shoppingCart)).MustHaveHappened();
+            useCase.AddProduct(shoppingCart.Id, productCode);
+
+            A.CallTo(() => cartRepository.Save(shoppingCart)).MustHaveHappened();
         }
 
         [Then(@"the shopping cart price should be (.*)")]
@@ -60,8 +61,10 @@ namespace ShoppingCartTests
             ICartQueries cartQueries = A.Fake<ICartQueries>();
             ICartPriceService cartPriceService = new CartPriceService();
             Id id = new Id();
-            A.CallTo(() => cartQueries.getProductsFromCart(id)).Returns(new List<CartProduct>(){new CartProduct("VOUCHER-DEMO",5M)});
-            CalculateCartPriceUseCase useCase = new CalculateCartPriceUseCase(cartPriceService,cartQueries);
+            A.CallTo(() => cartQueries.getProductsFromCart(id)).Returns(new List<CartProduct>()
+                { new CartProduct("VOUCHER-DEMO", 5M) });
+            CalculateCartPriceUseCase useCase = new CalculateCartPriceUseCase(cartPriceService, cartQueries);
             useCase.CalculateCartTotal(id).Should().Be(price);
         }
     }
+}
